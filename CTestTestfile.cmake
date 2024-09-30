03658d0c8f1f850fb687a7bede771745a75bf939
amaan <amaan.programmer1@gmail.com>
2024-09-30 12:07:52 +0530

    dont think i need this

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amaan/Documents/synaptic
-# Build directory: /home/amaan/Documents/synaptic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_all_tests "/home/amaan/Documents/synaptic/tests")
-set_tests_properties(run_all_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/amaan/Documents/synaptic/CMakeLists.txt;40;add_test;/home/amaan/Documents/synaptic/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
